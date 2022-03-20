171c80c84817f6801d2d1be80e66157c51e371e4
Gustavo <gustavoreisdev@gmail.com>
2022-03-20 01:02:50 -0300

    Refactoring css

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,16 +4,15 @@ import "./styles.scss";
 const Card = ({ imagem, titulo, descricao, valor }) => (
   <div className="card">
     <div className="content">
-      <img src={imagem} alt="produto" />
-
+      <div className="image">
+        <img src={imagem} alt="produto" />
+      </div>
       <div className="text">
         <h3>{titulo}</h3>
         <p>{descricao}</p>
       </div>
       <div className="actions">
-        <h2>
-          <span>R${valor}</span>
-        </h2>
+        <h2>R${valor}</h2>
         <button>
           <b>Presentear</b>
         </button>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -38,7 +38,7 @@ const Home = () => {
       }
     };
     getProducts();
-  }, [page]);
+  }, [page, gifts]);
 
   return (
     <div className="page">
